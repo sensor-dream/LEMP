e4eabf3f6bffe4292693882d3678d04801cd3fa1
sensor-dream <sensor-dream@sensor-dream.ru>
2021-06-19 23:32:29 +0700

    mailto: sensor-dream@sensor-dream.ru commiter: auto-commit

--- conf.bash
+++ conf.bash
@@ -8,22 +8,8 @@
 ## @Copyright © Sensor-Dream Boxed System (SDBS). Contacts: sensor-dream@sensor-dream.ru
 ## @License: http://www.apache.org/licenses/LICENSE-2.0
 ## @Site: https://sensor-dream.ru
-## @File: recompile.bash
-## @Creation date file: 19.06.2021, 22:15:17
+## @File: conf.bash
+## @Creation date file: 19.06.2021, 23:11:30
 
-while [ -z ${os_name} ]; do
-
-    cd ../
-
-    if [ -f conf.bash ]; then
-        . conf.bash
-    else
-        if [ -f CNAME ]; then
-            echo "Not search configuration file - conf.bash"
-            exit 1
-        fi
-    fi
-
-done
-
-echo "${os_name}"
+declare -rx os_name="$(cat /etc/*-release | awk -F'=' '/^NAME=/ { print $2 }')"
+declare -rx repo_path="$(pwd)"

792d0c448331426a9bc016d6fc4ec107f54e0401
sensor-dream <sensor-dream@sensor-dream.ru>
2021-06-19 22:17:24 +0700

    mailto: sensor-dream@sensor-dream.ru commiter: auto-commit

---
+++ nginx/recompile.bash
@@ -0,0 +1,12 @@
+#!/bin/env bash
+# -*- coding: utf-8 -*-
+#
+## @Author: sensor-dream
+## @Email: sensor-dream@sensor-dream.ru
+## @Copyright © sensor-dream. All rights reserved. Contacts: sensor-dream@sensor-dream.ru
+## @Copyright © sensor-dream. All rights reserved. Contacts: sensor-dream@sensor-dream.ru
+## @Copyright © Sensor-Dream Boxed System (SDBS). Contacts: sensor-dream@sensor-dream.ru
+## @License: http://www.apache.org/licenses/LICENSE-2.0
+## @Site: https://sensor-dream.ru
+## @File: recompile.bash
+## @Creation date file: 19.06.2021, 22:15:17
